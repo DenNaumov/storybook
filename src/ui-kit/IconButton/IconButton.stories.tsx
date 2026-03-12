11fff77261ce72f66c3053b2c62c049214bbbba7
Denis <>
2026-03-12 23:49:26 +0300

    Обновить сторис IconButton

--- src/ui-kit/IconButton/IconButton.stories.tsx
+++ src/ui-kit/IconButton/IconButton.stories.tsx
@@ -1,7 +1,7 @@
 import type { ComponentProps } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { IconButton } from './IconButton';
-import { ResizableIcon } from '../Icon/IconWrappers';
+import { Icon24, ResizableIcon } from '../Icon/IconWrappers';
 import { ResizableIcons, type ResizableIconKeys } from '../Icon/packs/resizable';
 
 const resizableIconNames = Object.keys(ResizableIcons) as ResizableIconKeys[];
@@ -54,12 +54,7 @@ const meta: Meta<IconButtonStoryArgs> = {
 export default meta;
 type Story = StoryObj<IconButtonStoryArgs>;
 
-const NotificationIcon = (size: number) => (
-    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M18 8C18 6.4087 17.3679 4.88258 16.2426 3.75736C15.1174 2.63214 13.5913 2 12 2C10.4087 2 8.88258 2.63214 7.75736 3.75736C6.63214 4.88258 6 6.4087 6 8C6 15 3 17 3 17H21C21 17 18 15 18 8Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M13.73 21C13.5542 21.3031 13.3019 21.5547 12.9982 21.7295C12.6946 21.9044 12.3504 21.9965 12 21.9965C11.6496 21.9965 11.3054 21.9044 11.0018 21.7295C10.6981 21.5547 10.4458 21.3031 10.27 21" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
-);
+const NotificationsIcon = <Icon24 icon="Notifications" size={24} />;
 
 export const Playground: Story = {
     args: {
@@ -133,15 +128,15 @@ export const Showcase: Story = {
             <div style={{ gridColumn: '1 / 4', textAlign: 'center', margin: '32px 0 16px', opacity: 0.8 }}>Badge=Yes</div>
 
             <div style={{ opacity: 0.6 }}>Default</div>
-            <center><IconButton icon={NotificationIcon(24)} buttonSize="m" iconSize="m" badgeCount="9" /></center>
+            <center><IconButton icon={NotificationsIcon} buttonSize="m" iconSize="m" badgeCount="9" /></center>
             <div />
 
             <div style={{ opacity: 0.6 }}>Pressed</div>
-            <center><IconButton icon={NotificationIcon(24)} buttonSize="m" iconSize="m" badgeCount="9" pressed /></center>
+            <center><IconButton icon={NotificationsIcon} buttonSize="m" iconSize="m" badgeCount="9" pressed /></center>
             <div />
 
             <div style={{ opacity: 0.6 }}>Disabled</div>
-            <center><IconButton icon={NotificationIcon(24)} buttonSize="m" iconSize="m" badgeCount="9" disabled /></center>
+            <center><IconButton icon={NotificationsIcon} buttonSize="m" iconSize="m" badgeCount="9" disabled /></center>
             <div />
         </div>
     ),
